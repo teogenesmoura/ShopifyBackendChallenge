0de10e9df0d181129847ab0808e9d27a4a20403d
Teogenes Moura <teogenesmoura@Teogeness-MacBook-Pro.local>
2019-01-19 13:28:36 -0200

    fixing database configuration issues

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_01_18_222101) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carts", force: :cascade do |t|
     t.string "title"
     t.float "total_amount"
