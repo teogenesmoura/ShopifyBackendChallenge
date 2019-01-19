2f6eba0570ce8bfc944270213dedfae559d96147
Teogenes Moura <teogenesmoura@Teogeness-MacBook-Pro.local>
2019-01-19 17:49:05 -0200

    added unit tests to cartsController

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -5,16 +5,6 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
     @cart = carts(:one)
   end
 
-  # test "should get index" do
-  #   get carts_url
-  #   assert_response :success
-  # end
-
-  # test "should get new" do
-  #   get new_cart_url
-  #   assert_response :success
-  # end
-
   test "should create cart" do 
     assert_difference('Cart.count') do 
       post cart_url, params: {title: 'Test Cart'}
@@ -28,35 +18,32 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
     assert_not_equal last, newCart
   end 
 
+  test "should create a cart" do 
+    carts_before = Cart.count 
+    post cart_url, params: {title: 'New Cart'}
+    carts_after = Cart.count 
+    assert_not_equal carts_before, carts_after 
+  end 
 
-  # test "should create cart" do
-  #   assert_difference('Cart.count') do
-  #     post carts_url, params: { cart: {  } }
-  #   end
+  test "should add product to a cart" do 
+    #to guarantee there are at least 1 product and 1 cart in the db
+    post product_url, params: { title: 'Test product', price: 1, inventory_count: 1}
+    post cart_url, params: {title: 'Test Cart'} 
+    product_id = Product.last.id
+    cart_id = Cart.last.id 
+    post "/addProductToCart", params: {cartId: cart_id, productId: product_id}
+    cart_total_amount = Cart.find(cart_id).total_amount 
+    assert_not_equal cart_total_amount, 0.0
+  end 
 
-  #   assert_redirected_to cart_url(Cart.last)
-  # end
-
-  # test "should show cart" do
-  #   get cart_url(@cart)
-  #   assert_response :success
-  # end
-
-  # test "should get edit" do
-  #   get edit_cart_url(@cart)
-  #   assert_response :success
-  # end
-
-  # test "should update cart" do
-  #   patch cart_url(@cart), params: { cart: {  } }
-  #   assert_redirected_to cart_url(@cart)
-  # end
-
-  # test "should destroy cart" do
-  #   assert_difference('Cart.count', -1) do
-  #     delete cart_url(@cart)
-  #   end
-
-  #   assert_redirected_to carts_url
-  # end
+  test "should checkout a cart" do 
+    post product_url, params: { title: 'Test product', price: 1, inventory_count: 1}
+    post cart_url, params: {title: 'Test Cart'} 
+    product_id = Product.last.id
+    cart_id = Cart.last.id  
+    post "/addProductToCart", params: {cartId: cart_id, productId: product_id}
+    post "/checkout", params: {cartId: cart_id}
+    amount_charged = JSON.parse(@response.body)["amount_charged"]
+    assert_equal 1,amount_charged
+  end 
 end
