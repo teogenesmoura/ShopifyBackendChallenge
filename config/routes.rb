91be85ed2cbb143a1aa34810cfd4f70df4e43f4a
Teogenes Moura <teogenesmoura@Teogeness-MacBook-Pro.local>
2019-01-19 17:56:20 -0200

    added documentation for the destroy method on Cart instance and fixed routing bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	post "/cart" => "carts#create"
 	get  "/carts" => "carts#index"
+	delete "/cart" => "carts#destroy"
 	get  "/cart/:id" => "carts#getCartById"
 	post "/addProductToCart" => "carts#addProductToCart"
 	post "/product" => "products#create"
